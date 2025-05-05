e8a8885534ccc880487374c88f3367135602d609
chagile <chagile@ac.sce.ac.il>
2025-05-05 23:49:04 +0300

    new file with log in and sign out functions

---
+++ functions.py
@@ -0,0 +1,70 @@
+import os
+import pickle
+import classes
+#from classes import Candidate
+
+def open_file_to_write(dictionary):
+    try:
+        with open("users.txt", "wb") as f:
+            pickle.dump(dictionary, f)
+    except FileNotFoundError:
+        print('File not found')
+    except Exception as e:
+        print(f'Error with opening the file {e}')
+    return
+
+def open_file_to_read():
+    dictionary = {}
+    try:
+        with open("users.txt", "rb") as f:
+            dictionary = pickle.load(f)
+    except FileNotFoundError:
+        print('File not found')
+    except Exception as e:
+        print(f'Error with opening the file {e}')
+    return dictionary
+
+def sign_out():
+    dict_ = open_file_to_read()
+    full_name = input('Enter your full name: ')
+    age = int(input('Enter your age: '))
+    while True:
+        try:
+            username = input('Username: ')
+            if username in dict_:
+                print('try to creat a username that does not exist')
+            else:
+                password = input('Password: ')
+                break
+        except ValueError:
+            print('Try again')
+    while True:
+        try:
+            choice = int(input('Press 1 to Employer profile and 2 to Candidate profile: '))
+            if choice == 1:
+                employer_user = classes.Employer(username, password, full_name, age)
+                dict_[username] = employer_user
+                break
+            if choice == 2:
+                candidate_user = classes.Candidate(username, password, full_name, age)
+                dict_[username] = candidate_user
+                break
+        except ValueError:
+            print('Choose again')
+
+    open_file_to_write(dict_)
+    return True
+
+def log_in():
+    while True:
+        username = input('Username: ')
+        dict_ = open_file_to_read()
+        if username in dict_:
+            password = input('Password: ')
+            user = dict_[username]
+            if password != user.password:
+                print('Wrong password!')
+            else:
+                return True
+        else:
+            print('Wrong username! Try again')
